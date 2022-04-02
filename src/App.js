377a0bce206269a994e2bf733c12e38f1e7713e4
Habidhossen <habidhossen2@gmail.com>
2022-04-03 01:04:02 +0600

    code added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import About from "./components/About/About";
 import CoinDetail from "./components/CoinDetail/CoinDetail";
 import Coins from "./components/Coins/Coins";
+import BdAddress from "./components/Contact/BdAddress";
 import Contact from "./components/Contact/Contact";
+import UsAddress from "./components/Contact/UsAddress";
 import Header from "./components/Header/Header";
 import HomePage from "./components/HomePage/HomePage";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
@@ -16,7 +18,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/coins" element={<Coins />} />
         <Route path="/coin-detail/:id" element={<CoinDetail />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />}>
+          <Route path="bd-address" element={<BdAddress />} />
+          <Route path="us-address" element={<UsAddress />} />
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Welcome from Contact Page</h1>
-    </div>
+    <section className="px-4 mt-12 mx-auto max-w-7xl md:px-2">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-xl font-semibold text-center md:text-3xl ">
+          Contact Us, Our Office Address Down Below
+        </h1>
+
+        <div className="flex justify-center items-center">
+          <Link
+            to="bd-address"
+            className="  py-1 px-3 text-white mt-3 mx-1 bg-sky-700 rounded-full w-auto sm:mb-0"
+          >
+            BD Address
+          </Link>
+          <Link
+            to="us-address"
+            className="  py-1 px-3 text-white mt-3 mx-1 bg-sky-700 rounded-full w-auto sm:mb-0"
+          >
+            US Address
+          </Link>
+        </div>
+        <div className="flex justify-center items-center w-full mt-24">
+          <Outlet />
+        </div>
+      </div>
+    </section>
   );
 };
 
