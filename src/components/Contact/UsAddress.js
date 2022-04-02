3e039c54ca33a1b958e0d6f9bbe95a5bd62947d4
Habidhossen <habidhossen2@gmail.com>
2022-04-03 02:10:40 +0600

    bug fixed

--- src/components/Contact/UsAddress.js
+++ src/components/Contact/UsAddress.js
@@ -7,7 +7,7 @@ const UsAddress = () => {
         <div>
           <h1>Street: 1203 SN Street</h1>
           <h1>City: New York</h1>
-          <h1>Phone: +1(607)2333333</h1>
+          <h1>Phone: +1(607)0000000</h1>
           <h1>Email: contact.usa@cc.com</h1>
         </div>
         <div>
